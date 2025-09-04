4295c6e86ca4ea4a71ed26c503deec9810d72305
Daniel Arrizza <daniel.arrizza@gmail.com>
2025-09-04 09:57:25 -0400

    update react router deps

--- app/routes/_seo+/sitemap[.]xml.ts
+++ app/routes/_seo+/sitemap[.]xml.ts
@@ -1,10 +1,9 @@
+import { getDomainUrl } from '#app/utils/misc'
 import { generateSitemap } from '@nasa-gcn/remix-seo'
 import type { LoaderFunctionArgs, ServerBuild } from 'react-router'
-import { getDomainUrl } from '#app/utils/misc'
 
 export async function loader({ context, request }: LoaderFunctionArgs) {
 	const serverBuild = (await context.serverBuild) as ServerBuild
-	// @ts-expect-error - route can be undefined
 	return generateSitemap(request, serverBuild.routes, {
 		headers: {
 			'Cache-Control': `public, max-age=${60 * 5}`,
